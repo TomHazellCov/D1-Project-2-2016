2fdf30ecb973ebc898dee3c421bc3a146e20dbda
Edvinas Kilbauskas <edvinaskilbauskas@gmail.com>
2016-01-26 09:49:11 +0000

    Deleted backup files, added folders, code is now runnable

--- Main.py
+++ Main.py
@@ -4,26 +4,46 @@ from kivy.properties import NumericProperty, ReferenceListProperty,\
     ObjectProperty
 from kivy.vector import Vector
 from kivy.clock import Clock
+from kivy.core.window import Window
 
+class Item:
+	
+	def __init__(self, name, type, price, quantity, position):
+		self.name = name
+		self.type = type
+		self.price = price
+		self.qty = quantity
+		self.pos = position
+		
+class Robot:
+	
+	def __init__(self, position):
+		pass
+		
+		
+class GameMap(Widget):
+	
+	def __init__(self, map):
+		pass
+		
+		
 class BargainHuntGame(Widget):
-    robot = ObjectProperty(None)
-    items = []
-    
-    def __init__(self):
-		
-
-    def update(self, dt):
-        
-    def on_touch_move(self, touch):
-		
+	items = []
+	
+	def update(self, dt):
+		pass
+	
+	def on_touch_down(self, touch):
+		print("X: " + str(touch.pos[0]) + " Y: " + str(touch.pos[1]))
 
 class BargainHuntApp(App):
-    def build(self):
-        game = BargainHuntGame()
-        game.serve_ball()
-        Clock.schedule_interval(game.update, 1.0 / 60.0)
-        return game
-
+	def build(self):
+		game = BargainHuntGame()
+		
+		Window.size = (1024,768)
+		Clock.schedule_interval(game.update, 1.0 / 60.0)
+		
+		return game
 
 if __name__ == '__main__':
-    BargainHuntApp().run()
+	BargainHuntApp().run()
