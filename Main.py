9a8299076382b44a042ed35e9bbf65c9527506a9
Edvinas Kilbauskas <edvinaskilbauskas@gmail.com>
2016-01-27 09:15:24 +0000

    Added images

--- Main.py
+++ Main.py
@@ -1,19 +1,34 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.uix.image import Image
+from kivy.uix.floatlayout import FloatLayout
 from kivy.properties import NumericProperty, ReferenceListProperty,\
     ObjectProperty
+    
 from kivy.vector import Vector
 from kivy.clock import Clock
 from kivy.core.window import Window
-
-class Item:
+from kivy.lang import Builder
 	
-	def __init__(self, name, type, price, quantity, position):
+class Background(Image):
+	pass
+
+class Item(Image):
+	
+	def __init__(self, name, type, price, qty, pos):
 		self.name = name
 		self.type = type
 		self.price = price
-		self.qty = quantity
-		self.pos = position
+		self.qty = qty
+		
+		super(Item,self).__init__()
+		
+		
+		
+		
+	def __str__(self):
+		return "Name: " + self.name + '\n' + "Type: " + self.type + '\n' + "Price: " + str(self.price) + '\n' + "Qty: " + str(self.qty)
 		
 class Robot:
 	
@@ -21,29 +36,45 @@ class Robot:
 		pass
 		
 		
-class GameMap(Widget):
-	
-	def __init__(self, map):
-		pass
 		
-		
-class BargainHuntGame(Widget):
+class GameScreen(FloatLayout):
 	items = []
+	def __init__(self, app):
+		super(GameScreen,self).__init__()
+		self.app = app
 	
 	def update(self, dt):
 		pass
 	
 	def on_touch_down(self, touch):
-		print("X: " + str(touch.pos[0]) + " Y: " + str(touch.pos[1]))
+		pass
 
-class BargainHuntApp(App):
+class MainApp(App):
 	def build(self):
-		game = BargainHuntGame()
 		
-		Window.size = (1024,768)
-		Clock.schedule_interval(game.update, 1.0 / 60.0)
+		self.root = GameScreen(self)
+		item = Item("3","3",3,4,Vector(10,10))
 		
-		return game
+		print(self.root.size)
+		
+		self.root.add_widget(Background())
+		self.root.add_widget(item)
+
+		Clock.schedule_interval(self.root.update, 1.0 / 60.0)
+		return self.root
+
+		
+	def update(self, dt):
+		pass
 
 if __name__ == '__main__':
-	BargainHuntApp().run()
+	MainApp().run()
+	
+	
+	
+	
+	
+	
+	
+	
+	
