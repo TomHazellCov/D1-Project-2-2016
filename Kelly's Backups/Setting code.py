933cc82a663a0f52c17578cf193d0d4a37eaf864
Sam Kelly <kellys28@coventry.ac.uk>
2016-02-15 10:38:34 +0000

    Uploading my back ups

---
+++ Kelly's Backups/Setting code.py
@@ -0,0 +1,41 @@
+"""Allows user to select settings"""
+
+
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+
+
+class Example(QWidget):
+    
+    def __init__(self):
+        super().__init__()
+        
+        self.initUI()
+
+
+def __init__(self):
+    super().__init__()
+    
+    self.initUI()
+
+
+def initUI(self):
+    self = QWidget()
+    self.resize(500, 500)
+    self.move(1200, 0)
+    self.setWindowTitle('Settings')
+    self.setWindowIcon(QIcon('settingicon.png'))
+    qbtn = QPushButton('Done', self)
+    qbtn.clicked.connect(QCoreApplication.instance().quit)
+    qbtn.resize(qbtn.sizeHint())
+    qbtn.move(100, 50)       
+    self.show()
+
+    
+    sys.exit(app.exec_())
+    
+if __name__ == '__main__':
+    
+    app = QApplication(sys.argv)
