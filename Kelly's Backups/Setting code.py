cf751a5d2c28ae80c88655a50d88209da7e2f435
Kellise <Samkelly16@hotmail.co.uk>
2016-02-17 09:30:05 +0000

    Update Setting code.py

--- Kelly's Backups/Setting code.py
+++ Kelly's Backups/Setting code.py
@@ -7,35 +7,34 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 
 
-class Example(QWidget):
-    
+class Setting_Code(QWidget):
+
+
     def __init__(self):
         super().__init__()
+
         
         self.initUI()
 
 
-def __init__(self):
-    super().__init__()
-    
-    self.initUI()
+    def initUI(self):
+        self = QWidget()
+        self.resize(500, 500)
+        self.move(1200, 0)
+        self.setWindowTitle('Settings')
+        self.setWindowIcon(QIcon('settingicon.png'))
+        qbtn = QPushButton('Done', self)
+        qbtn.clicked.connect(QCoreApplication.instance().quit)
+        qbtn.resize(qbtn.sizeHint())
+        qbtn.move(100, 50)       
+        self.show()
 
-
-def initUI(self):
-    self = QWidget()
-    self.resize(500, 500)
-    self.move(1200, 0)
-    self.setWindowTitle('Settings')
-    self.setWindowIcon(QIcon('settingicon.png'))
-    qbtn = QPushButton('Done', self)
-    qbtn.clicked.connect(QCoreApplication.instance().quit)
-    qbtn.resize(qbtn.sizeHint())
-    qbtn.move(100, 50)       
-    self.show()
-
-    
-    sys.exit(app.exec_())
-    
+        
+        sys.exit(app.exec_())
+        
 if __name__ == '__main__':
     
     app = QApplication(sys.argv)
+
+    ex = Setting_Code()
+    sys.exit(app.exec_()) 
